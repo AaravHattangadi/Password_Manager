ad4b9253468d7288de5443f2c9d179e0497ac4b4
aarav123-android <69454819+aarav123-android@users.noreply.github.com>
2020-11-17 23:02:25 +0530

    Added add password feature; Added node-echo;

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ An attempt at making a Password Manager in Node.js
 Warning: I am making this just for fun! This is *NOT* Secure
 
 Todo
-  [ ] Ask user for name of website
-  [ ] Ask user for username
-  [ ] Ask user for password
-  [ ] Create file based on name of website
-  [ ] Show that it is done
+  [x] Ask user for name of website
+  [x] Ask user for username
+  [x] Ask user for password
+    [x] Create file based on name of website
+    [x] Show that it is done
   
   [ ] Be able to list all passwords
   
---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const inquirer = require("inquirer");
+var echo = require('node-echo');
+
+echo("(If you have two accounts, DO NOT write the same name in this field! The first instance will be overwritten.")
+
+inquirer
+  .prompt([
+    {
+      type: "text",
+      name: "WebsiteName",
+      message: "What is the name of the website?",
+      default: "",
+    },
+    {
+      type: "text",
+      name: "Username",
+      message: "What is your username?",
+      default: "",
+    },
+    {
+      type: "password",
+      name: "Password",
+      message: "What is your password?",
+      default: "",
+    },
+  ])
+  .then((answers) => {
+    var fileName = answers.WebsiteName;
+    var webName = answers.WebsiteName;
+    var userName = answers.Username;
+    var passName = answers.Password;
+
+    process.chdir('Passwords');
+    fs.writeFileSync(`${fileName}.txt`, `${webName} \n ${userName} \n ${passName}`);
+    echo("Password Added!");
+  });
